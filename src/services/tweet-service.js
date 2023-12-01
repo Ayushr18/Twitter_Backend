eeee0c83d19f048b4eab8fd6e647ddec9660519a
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-12-01 23:45:54 +0530

    Added tweet service with hashing processing

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,16 +1,28 @@
-const {TweetRepository} = require('../repository/index')
+const {TweetRepository, HashtagRepository} = require('../repository/index')
 
 class TweetService {
     constructor() {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g);
-        tags = tags.map((tag) => tag.substring(1));
-        console.log(tags);
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1));
         const tweet = await this.tweetRepository.create(data);
+        let alreadypresentTags = await this.hashtagRepository.findByName(tags);
+        let titleOfPresenttags = alreadypresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresenttags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadypresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save(); 
+            
+        }); 
+  
         return tweet;
     }
 }
