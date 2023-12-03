94a738fa83ed7c0e8933693b0b01b296e15f6430
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-12-03 15:00:47 +0530

    Added basic crid repo and extended it in likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,21 +1,15 @@
 import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
 
-class TweetRepository {
-
+class TweetRepository extends CrudRepository{
+    constructor() {
+        super(Tweet);
+    }
 
     async create(data) {
         try {
             const tweet = await Tweet.create(data);
             return tweet;
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async get(id) {
-        try {
-           const tweet = await Tweet.findById(id);
-           return tweet; 
         } catch (error) {
             console.log(error)
         }
@@ -27,15 +21,6 @@ class TweetRepository {
             return tweet;
         } catch (error) {
             console.log(error);
-        }
-    }
-
-    async destroy(id) {
-        try {
-           const tweet = await Tweet.findByIdAndRemove(id);
-           return tweet; 
-        } catch (error) {
-            console.log(error)
         }
     }
 
