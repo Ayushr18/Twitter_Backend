84a0f15788325a4a444d2f7bbe10b5db08d4b802
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-12-05 00:14:53 +0530

    Updated Comment model

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,10 +5,21 @@ const commentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userEamil: {
-        type: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet, Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
