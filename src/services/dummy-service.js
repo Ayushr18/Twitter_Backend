8eceb9b1c33adda7b9b2d9874f14c8be29bef701
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-12-09 23:42:31 +0530

    Some Corrections in Unit Tests

--- src/services/dummy-service.js
+++ src/services/dummy-service.js
@@ -1,14 +1,12 @@
-import { execute } from "aws-sdk";
-
 export const helper = () => {
     const num = Math.floor(Math.random() * 10);
     return num % 2 == 0;
 }
 
 export const execute = () => {
-   // const result = helper();
-    if('Learning') {
-        return 'Learning JS';
+    const result = helper();
+    if(result) {
+        return 'Learning';
     } else {
         return 'Learning React Js'
     }
--- tests/services/dummy.test.js
+++ tests/services/dummy.test.js
@@ -1,6 +1,9 @@
-import {execute} from '../../src/services/dummy-service.js';
+import * as dummyFunction from '../../src/services/dummy-service.js';
 
 test('result is true and returns Learning JS', () => {
-    const result = execute();
-    expect(result).toBe('Learning')
+    const spy = jest.spyOn(dummyFunction, 'helper').mockImplementation(() => {
+        return true;
+    })
+    const result = dummyFunction.execute();
+    expect(result).toBe('Learning');
 });
