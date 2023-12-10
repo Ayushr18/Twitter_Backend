21f473ac3964617117e72c12550f4b2979c7262d
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-12-10 13:45:37 +0530

    Added Some Implementations of JEST

--- src/services/dummy-service.js
+++ src/services/dummy-service.js
@@ -1,13 +1,10 @@
-export const helper = () => {
-    const num = Math.floor(Math.random() * 10);
-    return num % 2 == 0;
-}
+import {helper} from './helper-service.js'
 
 export const execute = () => {
     const result = helper();
     if(result) {
-        return 'Learning';
+        return 'Learning JS';
     } else {
-        return 'Learning React Js'
+        return 'Learning React JS'
     }
 }
--- tests/services/dummy.test.js
+++ tests/services/dummy.test.js
@@ -1,9 +1,15 @@
-import * as dummyFunction from '../../src/services/dummy-service.js';
+import {execute} from '../../src/services/dummy-service.js';
+import {helper} from '../../src/services/helper-service.js';
+jest.mock('../../src/services/helper-service.js');
 
 test('result is true and returns Learning JS', () => {
-    const spy = jest.spyOn(dummyFunction, 'helper').mockImplementation(() => {
-        return true;
-    })
-    const result = dummyFunction.execute();
-    expect(result).toBe('Learning');
+    helper.mockReturnValue(true);
+    const result = execute();
+    expect(result).toBe('Learning JS');
+});
+
+test('result is true and returns Learning React JS', () => {
+    helper.mockReturnValue(false);
+    const result = execute();
+    expect(result).toBe('Learning React JS');
 });
